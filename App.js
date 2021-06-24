3fb4cf4bb6dddb551d9d168ff43e6f0a5f976124
garsan84 <jgrsan84@gmail.com>
2021-06-24 13:26:27 +0200

    coins showed on mobile emulator

--- App.js
+++ App.js
@@ -1,21 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react'
+import { View, Text, FlatList } from 'react-native'
 
-export default function App() {
+const App = () => {
+
+  const [coins, setCoins] = useState([])
+
+  const loadData = async () => {
+
+    const res = await fetch(
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+      );
+      const data = await res.json()
+     setCoins(data)
+  }
+
+
+useEffect(() =>{
+  console.log('loaded');
+  loadData();
+}, [])
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text>HELLO BOTS</Text>
+
+      <FlatList
+        data={coins}
+        renderItem={({item}) =>{
+          
+          return <Text>{item.name} </Text>
+        }}
+      />
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
